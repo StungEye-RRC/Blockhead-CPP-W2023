fba2d219429b0139479d5ee93703581b565db029
Kyle Geske <stungeye@gmail.com>
2023-03-13 12:06:25 -0500

    Added the endpoint, obstacle, and game mode cpp files, along with the bp versions of the player character and the game mode.

--- Source/BlockHeadCPPW2023/Actors/PlayerCharacter.cpp
+++ Source/BlockHeadCPPW2023/Actors/PlayerCharacter.cpp
@@ -7,11 +7,25 @@
 APlayerCharacter::APlayerCharacter() {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	UE_LOG(LogTemp, Warning, TEXT("Hello from C++ Player Character Constructor."));
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay() {
 	Super::BeginPlay();
+
+	int32 Answer = 42;
+	float Pi = 3.14;
+	FString Msg{"This is message."};
+
+	UE_LOG(LogTemp, Warning, TEXT("Hello from C++ Player Character Begin Play."));
+	UE_LOG(LogTemp, Warning, TEXT("Hello Logging int: %d"), Answer);
+	UE_LOG(LogTemp, Warning, TEXT("Hello Logging float: %f"), Pi);
+	UE_LOG(LogTemp, Warning, TEXT("Hello Loggin Fstring: %s"), *Msg);
+	GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Red,
+	                                 FString::Printf(
+		                                 TEXT("On Screen Hello Hello from C++ Player Character Begin Play: %d"),
+		                                 Answer));
 }
 
 // Called every frame
