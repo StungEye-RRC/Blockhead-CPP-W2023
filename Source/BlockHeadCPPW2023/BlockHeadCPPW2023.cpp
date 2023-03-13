38a33f252a14549de9fab25b8ef46e7086c6aff3
Kyle Geske <stungeye@gmail.com>
2023-03-13 11:13:17 -0500

    Project is now a C++ program, with a player character cpp and h file. (Add associated build files.)

---
+++ Source/BlockHeadCPPW2023/BlockHeadCPPW2023.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BlockHeadCPPW2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlockHeadCPPW2023, "BlockHeadCPPW2023" );
