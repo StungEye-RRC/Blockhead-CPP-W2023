132439d7a94b920de13d047fe00c009c2e8841ff
Kyle Geske <stungeye@gmail.com>
2023-03-13 13:29:03 -0500

    Updated the *.Build.cs file to include the enhanced input system and UMG.

--- Source/BlockHeadCPPW2023/BlockHeadCPPW2023.Build.cs
+++ Source/BlockHeadCPPW2023/BlockHeadCPPW2023.Build.cs
@@ -8,7 +8,7 @@ public class BlockHeadCPPW2023 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
