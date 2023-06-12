7bd8a0efa1797e4bd16ae33b78092d2b179f4998
Kyle Geske <stungeye@gmail.com>
2023-06-12 14:11:49 -0500

    Trying out the Rider IDE.

--- Source/BlockHeadCPPW2023/DebugHelper.h
+++ Source/BlockHeadCPPW2023/DebugHelper.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #define GLUTTON_LOG(Message) \
     UE_LOG(LogTemp, Warning, TEXT("%s"), *FString{Message}); \
     if (GEngine) GEngine->AddOnScreenDebugMessage(INDEX_NONE, 5.f, FColor::Yellow, Message)
